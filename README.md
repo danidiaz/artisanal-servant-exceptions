0c403f4a546b428a0dc4b1dcb5abc6bc2d8b97b9
Daniel Díaz <diaz_carrete@yahoo.com>
2023-04-01 11:59:40 +0200

    explanation

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
 # artisanal-servant-exceptions
 
+When a Servant handler throws an exception, we would like to have information
+about the chain of invocations that led to the exception. At that point we dont
+want to *recover* from the exception, but we would like to log it, providing as
+much context as possible.
+
+This repo is an example of a possible approach. If you run the server with
+`cabal run` and invoke the endpoint with
+
 ```
 curl -v -X POST localhost:8000
 ```
+
+then the following exception will be logged:
+
+```
+ExceptionWithStackTrace ["runFoo","runBar","runBaz"] user error (some exception)
+```
+
+Where `runFoo`, `runBar` and `runBaz` are annotations identifying functions in
+the call stack. Those annotations need to be explicitly added, they're not
+automatic. On the other hand, no changes to exception-throwing or
+exception-catching code are needed.
+
+## How it works
+
+For each individual request, we allocate an `IORef` which will contain the stack
+trace (a list of annotations). That `IORef` is passed as a reader environment.
+This is done in Servant's `hoistServer` function.
+
+When we enter an annotated function, we empty the `IORef`. We do the same when
+we exit an annotated function normally. 
+
+When we exit an annotated function _abnormally_, because of an exception, we add
+the function's annotation into the `IORef`. As the exception bubbles up, we
+build a chain of annotations.
+
+Once we are out of the handler, we catch the exception, inspect the `IORef`'s
+contents, and re-throw the exception wrapped in an `ExceptionWithStackTrace`.
+
+### A problem with this approach
+
+If we catch an exception coming from an annotated function and then throw an
+unrelated exception _without crossing an annotation frontier_ the old exception
+stays in the stack trace. 
+
+## Possible alternatives
+
+We could store the stack trace directly in the reader environment, without using
+a mutable `IORef`. 
+
+The stack trace would grow (using `local`) as we get into nested function calls.
+Exceptions would be caught and re-thrown as `ExceptionWithStackTrace` at the
+_earliest_ possible moment, without waiting until we exit the Servant handler.
+
+The problem with this is that it might interfere with exception-catching code.
+If one layer throws `IOException`s and some upper layer relies con catching
+them, converting the `IOException`s into `ExceptionWithStackTrace`s in some
+intermediate layer will break the logic.
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,46 @@
 module Main where
 
-import Control.Monad.Trans.Reader
 import Control.Exception
 import Control.Monad.IO.Class
-import Servant
+import Control.Monad.IO.Unlift
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+import Data.Function ((&))
 import Data.Functor
 import Network.Wai.Handler.Warp
-import Control.Monad.Trans.Class
-import StackTrace (StackTraceRef)
-import StackTrace qualified 
 import RIO
-import Data.Function ((&))
-import Control.Monad.IO.Unlift
+import Servant
+import StackTrace (StackTraceRef)
+import StackTrace qualified
 
-newtype Foo m = Foo { runFoo :: m () }
-newtype Bar m = Bar { runBar :: m () }
-newtype Baz m = Baz { runBaz :: m () }
+newtype Foo m = Foo {runFoo :: m ()}
+
+newtype Bar m = Bar {runBar :: m ()}
+
+newtype Baz m = Baz {runBaz :: m ()}
 
 makeFoo :: Bar m -> Foo m
-makeFoo bar = Foo { runFoo = runBar bar}
+makeFoo bar = Foo {runFoo = runBar bar}
 
 makeBar :: Baz m -> Bar m
-makeBar baz = Bar { runBar = runBaz baz}
+makeBar baz = Bar {runBar = runBaz baz}
 
--- makeBar :: Baz (RIO e) -> Bar (RIO e)
--- makeBar baz = Bar { runBar = do 
---         withRunInIO $ \runInIO -> do
---             _ <- try @IOException $ runInIO (runBaz baz)
---             pure ()
---         liftIO $ throwIO (userError "should be last in the stack trace, but it's not")
---     }
+-- | A defect of this approach. 
+-- 
+-- If we catch an exception coming from an annotated function (here 'runBaz')
+-- and then throw an unrelated exception _without crossing an annotation
+-- frontier_ the old exception stays in the stack trace. 
+makeBar' :: Baz (RIO e) -> Bar (RIO e)
+makeBar' baz =
+  Bar
+    { runBar = do
+        withRunInIO $ \runInIO -> do
+          _ <- try @IOException $ runInIO (runBaz baz)
+          throwIO (userError "should be last in the stack trace, but it isn't")
+    }
 
 makeBaz :: Baz (RIO e)
-makeBaz = Baz { runBaz = liftIO $ throwIO $ userError "some exception"}
+makeBaz = Baz {runBaz = liftIO $ throwIO $ userError "some exception"}
 
 type API = PostNoContent
 
@@ -41,9 +49,10 @@ makeFooServer foo = runFoo foo $> NoContent
 
 main :: IO ()
 main = do
-    let fooServer = makeFooServer foo
-        foo = makeFoo bar & \Foo {runFoo} -> Foo { runFoo = StackTrace.annotate "runFoo" runFoo}
-        bar = makeBar baz & \Bar {runBar} -> Bar { runBar = StackTrace.annotate "runBar" runBar}
-        baz = makeBaz & \Baz {runBaz} -> Baz { runBaz = StackTrace.annotate "runBax" runBaz}
-        hoistRequest action = Servant.Handler $ lift $ StackTrace.with $ runReaderT action
-    run 8000 $ serve (Proxy @API) $ hoistServer (Proxy @API) hoistRequest fooServer
+  let fooServer = makeFooServer foo
+      foo = makeFoo bar & \Foo {runFoo} -> Foo {runFoo = StackTrace.annotate "runFoo" runFoo}
+      bar = makeBar baz & \Bar {runBar} -> Bar {runBar = StackTrace.annotate "runBar" runBar}
+      -- bar = makeBar' baz & \Bar {runBar} -> Bar {runBar = StackTrace.annotate "runBar" runBar}
+      baz = makeBaz & \Baz {runBaz} -> Baz {runBaz = StackTrace.annotate "runBaz" runBaz}
+      hoistRequest action = Servant.Handler $ lift $ StackTrace.with $ runReaderT action
+  run 8000 $ serve (Proxy @API) $ hoistServer (Proxy @API) hoistRequest fooServer
