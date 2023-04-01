99e2ea76770841f777efa29cd856b276b8859d72
Daniel Díaz <diaz_carrete@yahoo.com>
2023-04-01 09:05:58 +0200

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import StackTrace (StackTraceRef)
 import StackTrace qualified 
 import RIO
 import Data.Function ((&))
+import Control.Monad.IO.Unlift
 
 newtype Foo m = Foo { runFoo :: m () }
 newtype Bar m = Bar { runBar :: m () }
@@ -21,6 +22,14 @@ makeFoo bar = Foo { runFoo = runBar bar}
 
 makeBar :: Baz m -> Bar m
 makeBar baz = Bar { runBar = runBaz baz}
+
+-- makeBar :: Baz (RIO e) -> Bar (RIO e)
+-- makeBar baz = Bar { runBar = do 
+--         withRunInIO $ \runInIO -> do
+--             _ <- try @IOException $ runInIO (runBaz baz)
+--             pure ()
+--         liftIO $ throwIO (userError "should be last in the stack trace, but it's not")
+--     }
 
 makeBaz :: Baz (RIO e)
 makeBaz = Baz { runBaz = liftIO $ throwIO $ userError "some exception"}
@@ -38,5 +47,5 @@ main = do
         foo = makeFoo bar & \Foo {runFoo} -> Foo { runFoo = StackTrace.annotate "runFoo" runFoo}
         bar = makeBar baz & \Bar {runBar} -> Bar { runBar = StackTrace.annotate "runBar" runBar}
         baz = makeBaz & \Baz {runBaz} -> Baz { runBaz = StackTrace.annotate "runBax" runBaz}
-        t action = Servant.Handler $ lift $ StackTrace.with $ runReaderT action
-    run 8000 $ serve (Proxy @API) $ hoistServer (Proxy @API) t fooServer
+        hoistRequest action = Servant.Handler $ lift $ StackTrace.with $ runReaderT action
+    run 8000 $ serve (Proxy @API) $ hoistServer (Proxy @API) hoistRequest fooServer
